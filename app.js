042a1c4f2326bf6fcb3576ba8d8d60c58990d37e
Piyanshu Mangla <piyanshumangla456@gmail.com>
2022-02-21 12:14:40 +0530

    adding .env

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
 const app = express();
-
+console.log(process.env.API_KEY)
 app.use(express.static('public'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 const secret = 'ThisisourLittleSecret';
-userSchema.plugin(encrypt, {secret: secret}, {encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret: process.env.SECRET}, {encryptedFields: ['password']});
 const User = new mongoose.model('User', userSchema);
 
 
